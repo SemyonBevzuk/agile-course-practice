45eaff4b350ea5d1bd1cbfd32a72b3fddccc3837
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-26 21:51:11 +0300

    Add View

---
+++ code/statistics-calculation/View/src/main/java/ru.unn.agile.StatisticsCalculation.view/Calculator.java
@@ -0,0 +1,18 @@
+package ru.unn.agile.StatisticsCalculation.view;
+
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ComboBox;
+//import javafx.scene.control.TextField;
+//import ru.unn.agile.StatisticsCalculation.model.ComplexNumber.Operation;
+//import ru.unn.agile.StatisticsCalculation.viewmodel.ViewModel;
+
+public class Calculator {
+
+    @FXML
+    void initialize() {
+
+    }
+}
